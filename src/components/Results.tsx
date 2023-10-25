ab575de0f48255fca293b80c1c6fef3e3140eac4
iudchenko <andrii.iudchenko@gmail.com>
2023-10-25 14:22:02 +0200

    fix: add nothing fouund case in search results components

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import Spinner from './Spinner';
-import Character from './Character';
-import { Person } from '../App';
+import React, { Component } from "react";
+import Spinner from "./Spinner";
+import Character from "./Character";
+import { Person } from "../App";
 
 interface ResultsProps {
   status: string;
@@ -16,13 +16,17 @@ export class Results extends Component<ResultsProps> {
     return (
       <main className="p-5 w-full max-w-3xl mx-auto flex flex-col items-start bg-gray-700/50 backdrop-blur-sm rounded-lg">
         <ul className="flex flex-col gap-2 w-full">
-          {this.props.status === 'loading' && <Spinner />}
-          {this.props.status === 'error' && <p>Something went wrong</p>}
-          {this.props.status === 'active' &&
+          {this.props.status === "loading" && <Spinner />}
+          {this.props.status === "error" && <p>Something went wrong</p>}
+          {this.props.status === "active" &&
             this.props.results.length > 0 &&
             this.props.results.map((character) => (
               <Character key={character.url} character={character} />
             ))}
+          {this.props.status === "active" &&
+            this.props.results.length === 0 && (
+              <p className="text-white">Nothing found</p>
+            )}
         </ul>
       </main>
     );

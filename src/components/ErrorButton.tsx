fc6bba445a56da144e9ec2bb6351e26641851bea
iudchenko <andrii.iudchenko@gmail.com>
2023-10-25 18:05:34 +0200

    fix: error button css fix

--- src/components/ErrorButton.tsx
+++ src/components/ErrorButton.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 interface ErrorButtonProps {}
 interface ErrorButtonState {
@@ -14,7 +14,7 @@ class ErrorButton extends Component<ErrorButtonProps, ErrorButtonState> {
   handleClick = () => {
     try {
       // Simulate an error
-      throw new Error('An error occurred');
+      throw new Error("An error occurred");
     } catch (error) {
       this.setState({ error: true });
     }
@@ -23,11 +23,11 @@ class ErrorButton extends Component<ErrorButtonProps, ErrorButtonState> {
   render() {
     if (this.state.error) {
       // This will trigger the error boundary
-      throw new Error('An error occurred in the render method.');
+      throw new Error("An error occurred in the render method.");
     }
 
     return (
-      <div className="p-5 absolute bottom-5 right-5">
+      <div className="p-5 fixed bottom-5 right-5">
         <button
           className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
           onClick={this.handleClick}
